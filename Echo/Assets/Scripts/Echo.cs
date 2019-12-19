52da5bfd54b5f814c66c61f48053d31d79e73ec2
qiweiyu <qi@weiyu.me>
2019-12-19 11:34:55 +0800

    echo

---
+++ Echo/Assets/Scripts/Echo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using UnityEngine.UI;
+
+public class Echo : MonoBehaviour
+{
+    Socket socket;
+
+    public InputField inputField;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Connection()
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect("127.0.0.1", 8888);
+    }
+
+    public void Send()
+    {
+        string sendStr = inputField.text;
+        byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);
+        socket.Send(sendBytes);
+
+        byte[] readBuff = new byte[1024];
+        int count = socket.Receive(readBuff);
+        string recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+
+        text.text = recvStr;
+        socket.Close();
+    }
+}
---
+++ EchoServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEp = new IPEndPoint(ipAddr, 8888);
+            listenfd.Bind(ipEp);
+            listenfd.Listen(0);
+            Console.WriteLine("Server Run!!!");
+
+            while (true)
+            {
+                Socket connfd = listenfd.Accept();
+                Console.WriteLine("Server accept");
+
+                byte[] readBuff = new byte[1024];
+
+                int count = connfd.Receive(readBuff);
+                string readStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+
+                Console.WriteLine("Receive: " + readStr);
+
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(readStr);
+
+                connfd.Send(sendBytes);
+            }
+        }
+    }
+}
